89f83b29afeb1d8774a7c729a1a3a00d4869efbf
Saransh Gupta <82395600+saranshusa@users.noreply.github.com>
2021-05-11 18:48:27 +0530

    Create Multiple File Rename Automation.py

---
+++ Multiple File Rename Automation.py
@@ -0,0 +1,26 @@
+# This program is an example of automation which renames multiple files present in a folder.
+# Eg. name0.png name1.png name2.png .......
+# Remember to use forward slash in the path name.
+
+import os
+
+def main():
+    ctr = 0
+    path = input('Enter path of the folder which contains the files to be renamed:\
+        \nEg. C:/Users/Saransh/Documents/\t:\t ')
+    
+    # Checks for path error
+    if '\\' in path:
+        print('\nERROR ! Use forward slash in path name (/)')
+    
+    base = input('\nEnter base file name:\t')
+    extension = input('\nEnter file extension name (Eg: .png):\t')
+    for filename in os.listdir(path):
+        destination = path + base + str(ctr) + extension
+        source = path + filename
+        os.rename(source, destination)
+        ctr += 1
+    print('\nSUCCESS!')
+
+if __name__ == '__main__':
+    main()        
