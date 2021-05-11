6d27723e89e1aa0cfedba40330a55d35eb116d25
Saransh Gupta <82395600+saranshusa@users.noreply.github.com>
2021-05-11 18:24:01 +0530

    Update GitHub Profile Picture Downloader.py

--- GitHub Profile Picture Downloader.py
+++ GitHub Profile Picture Downloader.py
@@ -1,16 +1,20 @@
 # This program downloads the github profile picture of the provided username.
 
-import requests
-from bs4 import BeautifulSoup as BS
+def main():
+    import requests
+    from bs4 import BeautifulSoup as BS
 
-user = input('Input GitHub Username: ')
-url = f'https://github.com/{user}'
-R = requests.get(url)
-soup = BS(R.content, 'html.parser')
-profileImage = soup.find('img', {'alt' : 'Avatar'})['src']
+    user = input('Input GitHub Username: ')
+    url = f'https://github.com/{user}'
+    R = requests.get(url)
+    soup = BS(R.content, 'html.parser')
+    profileImage = soup.find('img', {'alt' : 'Avatar'})['src']
 
-print(f'\nSuccessfully downloaded ! You may also go to the link below to open the image in web browser\n\n{profileImage}\n')
+    print(f'\nSuccessfully downloaded ! You may also go to the link below to open the image in web browser\n\n{profileImage}\n')
 
-# Auto downloads the image
-R = requests.get(profileImage, allow_redirects=True)
-open(f'{user}.jpg', 'wb').write(R.content)
+    # Auto downloads the image
+    R = requests.get(profileImage, allow_redirects=True)
+    open(f'{user}.jpg', 'wb').write(R.content)
+
+if __name__ == '__main__':
+    main()
