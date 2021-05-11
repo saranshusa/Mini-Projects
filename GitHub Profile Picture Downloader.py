c76e5f11bfae1db305f0fe0bb25de9683a3d9f62
Saransh Gupta <82395600+saranshusa@users.noreply.github.com>
2021-05-11 18:10:19 +0530

    project added

---
+++ GitHub Profile Picture Downloader.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+user = input('Input GitHub Username: ')
+url = f'https://github.com/{user}'
+R = requests.get(url)
+soup = BS(R.content, 'html.parser')
+profileImage = soup.find('img', {'alt' : 'Avatar'})['src']
+
+print(f'\nSuccessfully downloaded ! You may also go to the link below to open the image in web browser\n\n{profileImage}\n')
+
+# Auto downloads the image
+R = requests.get(profileImage, allow_redirects=True)
+open(f'{user}.jpg', 'wb').write(R.content)
